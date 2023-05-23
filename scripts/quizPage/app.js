6738aa1d24d3758537bebbd11cbb64b15203689d
Isabela Müller <isabelamuller1@hotmail.com>
2023-05-23 11:10:48 -0300

    feat(quiz-page): creation of the logic for the quiz

--- scripts/quizPage/app.js
+++ scripts/quizPage/app.js
@@ -1,6 +1,14 @@
 let counter = 0;
+let optionClicked;
 const checkboxes = document.querySelectorAll(".option-quiz");
 const nextQuestion = document.getElementById("next-question-btn");
+
+const option1 = document.getElementById("option-1");
+const option2 = document.getElementById("option-2");
+const option3 = document.getElementById("option-3");
+const option4 = document.getElementById("option-4");
+const options = [option1, option2, option3, option4];
+
 
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("click", (e) => {
@@ -12,7 +20,18 @@ checkboxes.forEach((checkbox) => {
   });
 });
 
+const handleCount = () => {
+    counter = parseInt(optionClicked.value) + counter;
+    optionClicked = ''
+};
+
 nextQuestion.addEventListener("click", (e) => {
   e.preventDefault();
-  
+  handleCount();
+});
+
+options.forEach((option) => {
+  option.addEventListener("click", (e) => {
+    optionClicked = e.target
+});
 });
