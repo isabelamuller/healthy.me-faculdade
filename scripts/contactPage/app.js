314aed142675313ed6cb2a081c0604d767a7a877
Isabela Müller <isabelamuller1@hotmail.com>
2023-06-13 15:59:11 -0300

    feat(schedule-page): creation of the form logic

---
+++ scripts/contactPage/app.js
@@ -0,0 +1,24 @@
+const nameForm = document.getElementById("name-form-field");
+const phoneForm = document.getElementById("phone-form-field");
+const emailForm = document.getElementById("email-form-field");
+const cityForm = document.getElementById("city-form-field");
+const submitBtn = document.getElementById("submit-btn-form");
+const dayWeekForm = document.getElementById("day-week-form-field");
+const specialistForm = document.getElementById("specialist-form-field");
+const hourForm = document.getElementById("hour-form-field");
+const messageForm = document.getElementById("message-form-field");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let formData = {
+    name: nameForm.value,
+    phone: parseInt(phoneForm.value),
+    email: emailForm.value,
+    cityState: cityForm.value,
+    day: dayWeekForm.value,
+    specialist: specialistForm.value,
+    timeOfDay: hourForm.value,
+    message: messageForm.value,
+  };
+  formData = {};
+});
